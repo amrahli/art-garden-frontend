e121cf127f2dbd75b3ac8232aa822d6a9105dfa0
amrahli <amrahli@hotmail.com>
2022-06-21 00:17:55 +0400

    header responsivity issue solved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState,useRef, useEffect} from 'react'
 import getContent from 'resources/translations'
 import ModalService from './Modal-Service'
 
@@ -9,20 +9,46 @@ const Header: React.FC = () => {
         e.preventDefault();
         setVisible("show")
     }
-
+    const header = document.getElementById('header')
+    window.addEventListener("resize",(e)=>{
+        const _window = window
+        if(window.innerWidth > 576){
+            header?.classList.remove("open")
+        }
+    })
+    const ref = useRef(null)
     const [visible,setVisible] = useState("hide")
+    const handleClick = () => {
 
+        const hamburger = document.querySelector("header#header .hamburger")
+        if(!header?.classList.contains("open")){
+            header?.classList.add("open")
+        }else{
+            header?.classList.remove("open")
+        }
+
+
+        const el2 = ref.current
+        console.log(el2)
+    }
     return (
-        <header>
+        <header ref={ref} id="header">
             <div className="row">
-                <div className="col-2">
+                <div className="col-11 col-sm-2">
                     <div className="logo">
                         <a href="/">
                             <img src="/artgarden-logo.png" alt="" />
                         </a>
                     </div>
                 </div>
-                <div className="col-10">
+                <div className="col-1 d-sm-none">
+                    <div className="hamburger" onClick={handleClick}>
+                        <span>|</span>
+                        <span>|</span>
+                        <span>|</span>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-10">
                     <div className="header-menu">
                         <ul>
                             <li><a href="/about">{translations.menu.aboutUs}</a></li>
